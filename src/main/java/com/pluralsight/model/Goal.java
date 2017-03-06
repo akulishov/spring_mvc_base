30114a436c69fb6415a90f67dbd310478f98294c
akulishov <a.a.kulishov1991@gmail.com>
2017-03-06 16:37:57 +0200

    Add dependencies in pom.xml. Add persistence context. Add persistence annotations on Activity and Goal.

--- src/main/java/com/pluralsight/model/Goal.java
+++ src/main/java/com/pluralsight/model/Goal.java
@@ -2,9 +2,17 @@ package com.pluralsight.model;
 
 import org.hibernate.validator.constraints.Range;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 
+@Entity
 public class Goal {
+
+	@Id
+	@GeneratedValue
+	private Long id;
 
 	@Range(min = 1, max = 120)
 	private int minutes;
@@ -16,5 +24,12 @@ public class Goal {
 	public void setMinutes(int minutes) {
 		this.minutes = minutes;
 	}
-	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 }
